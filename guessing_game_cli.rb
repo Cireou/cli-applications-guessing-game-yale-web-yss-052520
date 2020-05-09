ca29b2b9d126bd5cab39fc034cd8004aa65a8c61
Eric  Ou <eric.ou@yale.edu>
2020-05-09 21:39:21 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,11 @@
 def run_guessing_game
   ans = rand(6)+1
-  
+  puts "Guess a number between 1 and 6!"
+  input = gets.chomp
+  if input == ans 
+    puts "You guessed the correct number!"
+  elsif input == "exit"
+    puts "Goodbye!"
+  else 
+    puts "Sorry! The computer guessed #{ans}"
 end 
